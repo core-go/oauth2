e80171639a77ec39e2f62a032aa2b34055bd7105
Duc Nguyen <minhduc140583@gmail.com>
2020-09-19 11:04:52 +0700

    not include default_oauth2_integration_service

--- token_service.go
+++
@@ -1,6 +0,0 @@
-package oauth2
-
-type TokenService interface {
-	GenerateToken(payload interface{}, secret string, expiresIn int64) (string, error)
-	VerifyToken(tokenString string, secret string) (map[string]interface{}, int64, int64, error)
-}
